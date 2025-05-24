536b1fe13370d190c663140e38be76eb3dd4ca35
Addis Negash <anegash154@gmail.com>
2025-05-24 17:38:05 +0300

    the declartion of three important variables and addition of eventlistener to each button by using foreach method

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-let display=document.getElementsByClassName("screen")[0];
-let buttons=document.qqueryselectorAll("but");
+let display = document.getElementsByClassName("screen")[0];
+let buttons = document.qqueryselectorAll(".but");
 
+let current = "";
+let previous = "";
+let operator = "";
+
+buttons.forEach(item =>{
+    item.addEventlistenr('click',()=>{
+        const value=item.textContent;
+        const action=item.dataset.action;
+    
+    })
+});
